3d74e6ff88f7bb07e04cda16dd399eab66115b2f
dysen2018 <dysen@outlook.com>
2021-04-25 16:59:38 +0800

    日常提交。
    测试 SuperTextView .

--- app/src/main/java/com/dysen/lib/coil_test/CoilTestActivity.kt
+++ app/src/main/java/com/dysen/lib/coil_test/CoilTestActivity.kt
@@ -1,6 +1,13 @@
 package com.dysen.lib.coil_test
 
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.decode.SvgDecoder
+import coil.fetch.VideoFrameFileFetcher
+import coil.fetch.VideoFrameUriFetcher
 import coil.load
 import coil.transform.BlurTransformation
 import coil.transform.CircleCropTransformation
@@ -41,23 +48,41 @@ class CoilTestActivity : BaseActivity() {
             placeholder(R.drawable.ic_img_load_before)
             transformations(BlurTransformation(this@CoilTestActivity, 10f))
         }
-        iv4.load(imgUrl){
+
+        val imageLoader = ImageLoader.Builder(this)
+            .componentRegistry {
+                //① Gif（GifDecoder 支持所有 API 级别，但速度较慢，ImageDecoderDecoder 的加载速度快，但仅在 API 28 及更高版本可用）
+                if (SDK_INT >= 28) {
+                    add(ImageDecoderDecoder())
+                } else {
+                    add(GifDecoder())
+                }
+                //② SVG（如果请求的 MIME 类型是 image/svg+xml，则会自动检测并解码所有 SVG）
+                add(SvgDecoder(this@CoilTestActivity))
+                //③ 视频帧（仅支持 File 和 Uri）
+                add(VideoFrameFileFetcher(this@CoilTestActivity))
+                add(VideoFrameUriFetcher(this@CoilTestActivity))
+            }
+            .build()
+
+        iv4.load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg2018.cnblogs.com%2Fblog%2F600079%2F201905%2F600079-20190513102302965-941998498" +
+                ".gif&refer=http%3A%2F%2Fimg2018.cnblogs.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621676900&t=664eea55d92db2ae71091bb707d40fb1",imageLoader){
             crossfade(true)
             placeholder(R.drawable.ic_img_load_before)
-            transformations(CircleCropTransformation())
         }
-
-        iv5.load(File("https://player.vimeo.com/external/328940142.hd.mp4?s=1ea57040d1487a6c9d9ca9ca65763c8972e66bd4&profile_id=172")){
+        iv5.load("https://img.zcool.cn/community/01b0d857b1a34d0000012e7e87f5eb.gif",imageLoader){
             crossfade(true)
             placeholder(R.drawable.ic_img_load_before)
             error(R.drawable.ic_img_load_failed)
-            transformations(CircleCropTransformation(),BlurTransformation(this@CoilTestActivity, 10f))
+//            transformations(CircleCropTransformation())
         }
 
-        iv6.load("https://player.vimeo.com/external/328940142.hd.mp4?s=1ea57040d1487a6c9d9ca9ca65763c8972e66bd4&profile_id=172"){
+        iv6.load("http://www.iqiyi.com/v_19ruv3n460.html",imageLoader){
             crossfade(true)
             placeholder(R.drawable.ic_img_load_before)
-            transformations(BlurTransformation(this@CoilTestActivity, 10f), RoundedCornersTransformation(30f))
+            error(R.drawable.ic_img_load_failed)
+
+//            transformations(BlurTransformation(this@CoilTestActivity, 10f), RoundedCornersTransformation(30f))
         }
     }
 }
---
+++ app/src/main/java/com/dysen/lib/test/SuoerTextviewActy.kt
@@ -0,0 +1,19 @@
+package com.dysen.lib.test
+
+import android.os.Bundle
+import com.dysen.baselib.base.BaseActivity
+import com.dysen.lib.R
+
+
+/**
+ * @author dysen
+ * dy.sen@qq.com     4/25/21 2:28 PM
+ *
+ * Info：
+ */
+class SuoerTextviewActy: BaseActivity(){
+    override fun layoutId(): Int = R.layout.activity_supertextview
+
+    override fun initView(savedInstanceState: Bundle?) {
+    }
+}
